08a8635b0b10b3919ccbd82adb4f92cefeae0af5
Stony Bear <2724776621@qq.com>
2026-05-26 14:21:11 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TcampHomework
+
+Developed with Unreal Engine 5
---
+++ Source/TcampHomework/TcampHomeworkGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TcampHomeworkGameMode.h"
+
+ATcampHomeworkGameMode::ATcampHomeworkGameMode()
+{
+	// stub
+}
